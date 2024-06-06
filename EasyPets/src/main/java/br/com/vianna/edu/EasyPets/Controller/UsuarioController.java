e62bae50a5d37f10d5e71264b667b92c4a8b1235
nikao8 <nicolasmarquesjf@hotmail.com>
2024-06-06 20:19:52 -0300

    Subindo alteracoes no home para  o menu de usuarios, controller de usuario e pagina de listagem

---
+++ EasyPets/src/main/java/br/com/vianna/edu/EasyPets/Controller/UsuarioController.java
@@ -0,0 +1,64 @@
+package br.com.vianna.edu.EasyPets.Controller;
+
+import br.com.vianna.edu.EasyPets.Model.user.User;
+import br.com.vianna.edu.EasyPets.Model.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
+public class UsuarioController {
+
+    @Autowired
+    private UserRepository repository;
+
+    @GetMapping("/cadastroUsuario")
+    public String cadastroUsuario(Model model) {
+        model.addAttribute("usuario", new User());
+        return "cadastroUsuario";
+    }
+
+    @PostMapping("/usuario/cadastrar")
+    public String salvarUsuario(@ModelAttribute User usuario) {
+        repository.save(usuario);
+        return "redirect:/home";
+    }
+
+    @GetMapping("/listaUsuarios")
+    public String listarUsuarios(Model model) {
+        List<User> usuarios = repository.findAll();
+        model.addAttribute("usuarios", usuarios);
+        return "listaUsuarios";
+    }
+
+    @GetMapping("/usuario/remover/{id}")
+    public String removerUsuario(@PathVariable("id") final Long id ) {
+        repository.deleteById(id);
+        return "redirect:/listaUsuarios";
+    }
+
+    @GetMapping("/usuario/{id}")
+    @ResponseBody
+    public User buscarUsuario(@PathVariable("id") final Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    @PutMapping("/usuario/atualizar/{id}")
+    @ResponseBody
+    public User atualizarUsuario(@PathVariable Long id, @RequestBody User usuario) {
+        User usuarioExistente = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Usuario não encontrado"));
+        usuarioExistente.setNome(usuario.getNome());
+        usuarioExistente.setEmail(usuario.getEmail());
+        usuarioExistente.setCpf(usuario.getCpf());
+        usuarioExistente.setLogin(usuario.getLogin());
+        usuarioExistente.setSenha(usuario.getSenha());
+        usuarioExistente.setTipoUser(usuario.getTipoUser());
+        return repository.save(usuarioExistente);
+    }
+
+}
