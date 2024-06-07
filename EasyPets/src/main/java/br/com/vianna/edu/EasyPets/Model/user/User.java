d9874d1be13d33afffcd3dd046d6501c35c087ce
nikao8 <nicolasmarquesjf@hotmail.com>
2024-06-06 21:56:33 -0300

    finalizando cadastro de usuario

--- EasyPets/src/main/java/br/com/vianna/edu/EasyPets/Model/user/User.java
+++ EasyPets/src/main/java/br/com/vianna/edu/EasyPets/Model/user/User.java
@@ -3,6 +3,8 @@ package br.com.vianna.edu.EasyPets.Model.user;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDate;
+
 @Table(name = "users")
 @Entity(name = "User")
 @Getter
@@ -28,4 +30,61 @@ public class User {
 
     @Enumerated(EnumType.STRING)
     private EtipoUser tipoUser;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public EtipoUser getTipoUser() {
+        return tipoUser;
+    }
+
+    public void setTipoUser(EtipoUser tipoUser) {
+        this.tipoUser = tipoUser;
+    }
 }
